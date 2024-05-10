961670b3d74c3bc217489e6ae16d0c373081e2df
Stupid-Kimi <15381388023@163.com>
2024-05-10 02:00:30 +0000

    11

---
+++ check.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, k, z;
+vector <int> a, b;
+int main() {
+    freopen("in.txt" , "w", stdout);
+    n = rand() % 10, k = rand() % n;
+    cout << n << ' ' << k << '\n';
+    system("./T1 < in.txt > out.txt");
+    freopen("out.txt" , "r", stdin);
+    cin >> z >> z;
+    a.resize(n), b.resize(n);
+    for (int &i : a) cin >> i;
+    cin >> z;1
+    for (int &i : b) cin >> i;
+
+}
