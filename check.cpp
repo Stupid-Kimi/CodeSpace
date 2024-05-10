3057b27f1230198069fcc4276402b79781d84972
Stupid-Kimi <15381388023@163.com>
2024-05-10 02:02:50 +0000

    main

--- check.cpp
+++ check.cpp
@@ -2,6 +2,19 @@
 using namespace std;
 int n, k, z;
 vector <int> a, b;
+int qpow(int x, int y) {
+    if (y == 0)
+        return 1;
+    else if (y == 1)
+        return x;
+    else {
+        int n = qpow(x, y / 2);
+        if (y & 1)
+            return n * n * x;
+        else
+            return n * n;
+    }
+}
 int main() {
     freopen("in.txt" , "w", stdout);
     n = rand() % 10, k = rand() % n;
@@ -11,7 +24,10 @@ int main() {
     cin >> z >> z;
     a.resize(n), b.resize(n);
     for (int &i : a) cin >> i;
-    cin >> z;1
+    cin >> z;
     for (int &i : b) cin >> i;
-
+    for (int i = 0; i <= k; i++) {
+        long long sum1 = 0, sum2 = 0;
+        for (int i : a) 
+    }
 }
