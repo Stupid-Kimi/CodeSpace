636b441e796dc2d8af5fb8a3379f0b02dd10f266
Stupid-Kimi <15381388023@163.com>
2024-05-10 02:30:56 +0000

    main

---
+++ T3.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 1005;
+int n, k;
+double a[N];
+int main() {
+    cin >> n >> k;
+    for (int i = 1; i <= n; i++) cin >> a[i];
+    while(k--) {
+        
+    }
+}
